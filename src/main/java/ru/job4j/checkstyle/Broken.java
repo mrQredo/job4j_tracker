71279932e86eaf47630ff6d43d696c17aaddf227
Qredo <vggbratsk@gmail.com>
2026-06-09 22:11:02 +0700

    Правила Checkstyle [#504981 #564669]

---
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -0,0 +1,20 @@
+package ru.job4j.checkstyle;
+
+public class Broken {
+    public static final String NEW_VALUE = "";
+    private String surname;
+    private String name;
+    private final int sizeOfEmpty = 10;
+
+    public Broken() { }
+
+    public void echo() { }
+
+    public void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+    public void method(int a, int b, int c, int d, int e, int f, int g) { }
+}
