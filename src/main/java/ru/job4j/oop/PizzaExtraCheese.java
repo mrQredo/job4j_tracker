2b10b2ca2b5a9397d43adea59706c728f6493e2a
Qredo <vggbratsk@gmail.com>
2026-07-02 19:31:05 +0700

    Вызов переопределенного метода родителя - super.method ... [#504871]

---
+++ src/main/java/ru/job4j/oop/PizzaExtraCheese.java
@@ -0,0 +1,9 @@
+package ru.job4j.oop;
+
+public class PizzaExtraCheese extends Pizza {
+    private static final String name = " + extra cheese";
+
+    public String name() {
+        return "Just tasty pizza" + name;
+    }
+}
