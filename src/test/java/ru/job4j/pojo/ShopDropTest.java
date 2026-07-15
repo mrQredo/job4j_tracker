94bd37587ba449a34281c5878aac17dec5285778
Qredo <vggbratsk@gmail.com>
2026-07-15 22:53:13 +0700

    Синтаксис.

--- src/test/java/ru/job4j/pojo/ShopDropTest.java
+++ src/test/java/ru/job4j/pojo/ShopDropTest.java
@@ -3,8 +3,8 @@ package ru.job4j.pojo;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
-
 class ShopDropTest {
+
     @Test
     public void whenDropFirst() {
         Product[] products = new Product[2];
