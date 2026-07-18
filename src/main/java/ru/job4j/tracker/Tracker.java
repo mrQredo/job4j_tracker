0187ffa9eb3cf4044234afb50b54492190a73910
Qredo <vggbratsk@gmail.com>
2026-07-18 18:51:57 +0700

    7. Метод удаления заявки Tracker.delete [#211749 #565827]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -69,7 +69,7 @@ public class Tracker {
         int index = indexOf(id);
         boolean result = index != -1;
         if (result) {
-            System.arraycopy(items, indexOf(id) + 1, items, indexOf(id), items.length - indexOf(id) - 1);
+            System.arraycopy(items, index + 1, items, index, items.length - index - 1);
             items[size - 1] = null;
             size--;
         }
