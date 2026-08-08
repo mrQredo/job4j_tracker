85d61eca353b42e397d345d93f20217c90836ef9
Qredo <vggbratsk@gmail.com>
2026-08-08 14:06:37 +0700

    11.1. Реализация меню за счет шаблона стратегия. [#181780]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,7 +1,7 @@
 package ru.job4j.tracker;
 
 /**
- * Следующей задачей в нашем проекте будет отображение меню - ru.job4j.tracker.StartUI.
+ * Следующей задачей в нашем проекте будет отображение меню - ru.job4j.tracker. StartUI.
  * Этот объект будет управлять меню. В нем будет цикл, который будет опрашивать пользователя о выбранном пункте меню.
  * Если пользователь выбрал пункт "Выйти", то цикл завершается и программа закрывается.
  */
@@ -9,115 +9,36 @@ package ru.job4j.tracker;
 public class StartUI {
         
     //Метод init инициализирует приложение и запускает выполнение различных пользовательских действий.
-    public void init(Input input, Tracker tracker) {
+    public void init(Input input, Tracker tracker, UserAction[] actions) {
         boolean run = true;
         while (run) {
-            showMenu();
+            showMenu(actions);
             int select = input.askInt("Выбрать: ");
-            if (select == 0) {
-                createItem(input, tracker);
-            } else if (select == 1) {
-                findAllItems(tracker);
-            } else if (select == 2) {
-                replaceItem(input, tracker);
-            } else if (select == 3) {
-                deleteItem(input, tracker);
-            } else if (select == 4) {
-                findItemById(input, tracker);
-            } else if (select == 5) {
-                findItemByName(input, tracker);
-            } else if (select == 6) {
-                run = false;
-            }
-        }
-    }
-
-    //0. Создание новой заявки - createItem
-    public static void createItem(Input input, Tracker tracker) {
-        System.out.println("=== Создание новой заявки ===");
-        String name = input.askStr("Введите имя: ");
-        Item item = new Item(name);
-        tracker.add(item);
-        System.out.println("Добавленная заявка: " + item);
-    }
-
-    //1. Поиск всех заявок - findAllItems
-    public static void findAllItems(Tracker tracker) {
-        System.out.println("=== Вывод всех заявок ===");
-        Item[] items = tracker.findAll();
-        if (items.length > 0) {
-            for (Item item : items) {
-                System.out.println(item);
-            }
-        } else {
-            System.out.println("Хранилище еще не содержит заявок");
-        }
-    }
-
-    //2. Редактирование заявки - replaceItem
-    public static void replaceItem(Input input, Tracker tracker) {
-        System.out.println("=== Редактирование заявки ===");
-        int id = input.askInt("Введите id: ");
-        String name = input.askStr("Введите имя: ");
-        Item item = new Item(name);
-        if (tracker.replace(id, item)) {
-            System.out.println("Заявка изменена успешно.");
-        } else {
-            System.out.println("Ошибка замены заявки.");
-        }
-    }
-
-    //3. Удаление заявки - deleteItem
-    public static void deleteItem(Input input, Tracker tracker) {
-        System.out.println("=== Удаление заявки ===");
-        int id = input.askInt("Введите id: ");
-        Item item = tracker.findById(id);
-        tracker.delete(id);
-        System.out.println(item != null ? "Заявка удалена успешно." : "Ошибка удаления заявки.");
-    }
-
-    //4. Поиск по id - findItemById
-    public static void findItemById(Input input, Tracker tracker) {
-        System.out.println("=== Вывод заявки по id ===");
-        int id = input.askInt("Введите id: ");
-        Item item = tracker.findById(id);
-        if (item != null) {
-            System.out.println(item);
-        } else {
-            System.out.println("Заявка с введенным id: " + id + " не найдена.");
-        }
-    }
-
-    //5. Поиск по имени - findItemByNam
-    public static void findItemByName(Input input, Tracker tracker) {
-        System.out.println("=== Вывод заявок по имени ===");
-        String name = input.askStr("Введите имя: ");
-        Item[] items = tracker.findByName(name);
-        if (items.length > 0) {
-            for (Item item : items) {
-                System.out.println(item);
-            }
-        } else {
-            System.out.println("Заявки с именем: " + name + " не найдены.");
+            UserAction action = actions[select];
+            run = action.execute(input, tracker);
         }
     }
 
     //Метод showMenu выводит на экран меню доступных пользовательских действий.
-    private void showMenu() {
-        String[] menu = {
-                "Добавить новую заявку", "Показать все заявки", "Изменить заявку",
-                "Удалить заявку", "Показать заявку по id", "Показать заявки по имени",
-                "Завершить программу"
-        };
+    private void showMenu(UserAction[] actions) {
         System.out.println("Меню:");
-        for (int i = 0; i < menu.length; i++) {
-            System.out.println(i + ". " + menu[i]);
+        for (int index = 0; index < actions.length; index++) {
+            System.out.println(index + ". " + actions[index].name());
         }
     }
 
     public static void main(String[] args) {
         Input input = new ConsoleInput();
         Tracker tracker = new Tracker();
-        new StartUI().init(input, tracker);
+        UserAction[] actions = {
+                new CreateAction(),
+                new FindAllAction(),
+                new ReplaceAction(),
+                new DeleteAction(),
+                new FindByIdAction(),
+                new FindByNameAction(),
+                new ExitAction()
+        };
+        new StartUI().init(input, tracker, actions);
     }
 }
