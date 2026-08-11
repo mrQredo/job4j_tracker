19fa507b2acee02b0ee65e0320d99c78c93b9fb5
Qredo <vggbratsk@gmail.com>
2026-08-11 21:05:41 +0700

    0.4. Пользовательские исключения. [#219367]

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -1,24 +1,21 @@
 package ru.job4j.ex;
 
-import ru.job4j.tracker.Item;
-
 public class FindEl {
     public static int indexOf(String[] value, String key) throws ElementNotFoundException {
         int result = -1;
-        /* цикл fori, поскольку нам надо найти индекс искомого элемента в массиве */
         for (int i = 0; i < value.length; i++) {
             if (key.equals(value[i])) {
                 result = i;
                 return result;
             }
         }
-        throw new ElementNotFoundException("ElementNotFoundException");
+        throw new ElementNotFoundException("Element not found in the array.");
     }
 
     public static void main(String[] args) {
         String[] value = {"Gena", "Petya", "Stas"};
         try {
-            indexOf(value, "Gena");
+            indexOf(value, "Olga");
         } catch (ElementNotFoundException e) {
             e.printStackTrace();
         }
