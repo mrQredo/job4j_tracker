a13fb597381bb5bec67735776732fb4e8b8388f8
Qredo <vggbratsk@gmail.com>
2026-08-24 22:14:29 +0700

    Повторил материал.

---
+++ src/main/java/ru/job4j/my/UsageArrayList.java
@@ -0,0 +1,17 @@
+package ru.job4j.my;
+
+import java.util.ArrayList;
+
+public class UsageArrayList {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> names = new ArrayList<>();
+        names.add("Petr");
+        names.add("Ivan");
+        names.add("Stepan");
+        for(String string : names) {
+            System.out.println(string);
+        }
+    }
+}
